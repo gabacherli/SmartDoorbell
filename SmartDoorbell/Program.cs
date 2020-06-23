5d04ddc32eeb5deb9c6c2e6a93be84490847d2b7
Gabriel Abacherli Coelho <gabriel.coelho@ax4b.com>
2020-06-22 23:51:37 -0300

    Add project files.

---
+++ SmartDoorbell/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SmartDoorbell
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SendNotification();
+        }
+
+        private static async Task<HttpResponseMessage> SendNotification()
+        {
+            var client = new HttpClient();
+
+            var pushNotificationUri = "http://api.pushingbox.com/pushingbox?devid={0}";
+
+            var devId = "vC5BDEC64025A849";
+
+            var uri = string.Format(pushNotificationUri, devId);
+
+            var result = await client.GetAsync(uri);
+
+            return result;
+        }
+    }
+}
